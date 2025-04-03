63946eace60c79c6f0339f9cf34e24b7f976b6e2
agustin <agustingarcia280998@gmail.com>
2025-04-03 19:05:57 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import InputTag from './components/inputTag.vue'
+</script>
+
+<template>
+<InputTag/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/InputTag.vue
@@ -0,0 +1,19 @@
+<script>
+    export default {
+        data() {
+            return {
+                currentValue:"",
+                tags:[]
+            }
+        },
+    }
+</script>
+
+<template>
+<div class="inputTag">
+    <div class="tags"></div>
+    <input type="text"/>
+</div>
+</template>
+
+<style scoped></style> 
