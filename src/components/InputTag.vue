7275a49159febef0d75e52481919ae80420e1b6a
agustin <agustingarcia280998@gmail.com>
2025-04-05 17:22:11 -0300

    add functions

--- src/components/InputTag.vue
+++ src/components/InputTag.vue
@@ -1,19 +1,46 @@
 <script>
-    export default {
-        data() {
-            return {
-                currentValue:"",
-                tags:[]
-            }
-        },
+export default {
+  data() {
+    return {
+      currentValue: '',
+      tags: [],
     }
+  },
+
+  methods: {
+    // en uno validas la tecla que se presiona y en el otro validas el submit del form directamente
+    handleKeyDown(e) {
+      if (e.key === 'Backspace' && this.currentValue === '') {
+        this.tags.pop()
+      }
+    },
+    handleSubmit() {
+      if (this.currentValue !== '') {
+        const exists = this.tags.some((item) => item === this.currentValue)
+        if (!exists) {
+          this.tags.push(this.currentValue)
+          this.currentValue = ''
+        }
+      }
+    },
+    deleteTag(tag) {
+      this.tags = this.tags.filter((t) => t !== tag)
+    },
+  },
+}
 </script>
 
 <template>
-<div class="inputTag">
-    <div class="tags"></div>
-    <input type="text"/>
-</div>
+  <div class="inputTag">
+    <div class="tags">
+      <div class="tag" v-for="(tag, index) in tags" :key="index">
+        {{ tag }} <button @click="deleteTag(tag)">X</button>
+      </div>
+    </div>
+    <form @submit.prevent="handleSubmit">
+      <input type="text" v-model="currentValue" @keydown="handleKeyDown" />
+    </form>
+  </div>
 </template>
 
-<style scoped></style> 
+<style scoped></style>
